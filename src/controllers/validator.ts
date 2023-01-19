ebe30b3bd356b3379aca64db93845bb3a688f928
Sebastian009w <ia3266505@gmail.com>
2023-01-19 09:11:24 -0500

    Inicia proyecto credit card validation

---
+++ src/controllers/validator.ts
@@ -0,0 +1,9 @@
+class Validator {
+  constructor() {}
+
+  public isValid(ccn: string): void {}
+
+  public maskify(): void {}
+}
+
+export default Validator;
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+import Validator from './controllers/validator';
+
+const validator = new Validator();
+console.log(validator);
