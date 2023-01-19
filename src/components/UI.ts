3df6eb9d99309eaae711aea6508ff1bb972f90aa
Sebastian009w <ia3266505@gmail.com>
2023-01-19 09:58:41 -0500

    Se estiliza un poco la interfaz

--- src/components/UI.ts
+++ src/components/UI.ts
@@ -3,10 +3,10 @@ class UI {
 
   public card(): void {
     const $root = document.getElementById("app") as unknown as HTMLDivElement;
-    $root.className = 'bg-zinc-800 h-screen';
+    $root.className = 'bg-zinc-800 h-screen flex justify-center items-center';
 
     const $section = document.createElement("section");
-    $section.className = '';
+    $section.className = 'w-10 h-10 bg-red-800 p-10';
 
     const $card = document.createElement("div");
     $card.className = '';
