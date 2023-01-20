06567949dc3db09f8cf61719c857339aafac6863
Sebastian009w <ia3266505@gmail.com>
2023-01-20 10:21:19 -0500

    Se estiliza la card

--- src/components/UI.ts
+++ src/components/UI.ts
@@ -6,21 +6,24 @@ class UI {
     $root.className = "bg-zinc-800 h-screen flex justify-center items-center";
 
     const $container = document.createElement("section");
-
+    $container.className = "w-full";
     const $card = document.createElement("div");
-    $card.className = "w-96 h-56 rounded-md	 bg-zinc-500 shadow-lg shadow-zinc-500/50 flex flex-col items-start justify-end pb-5 gap-2";
+    $card.className =
+      "w-96 h-56 rounded-md	 bg-zinc-500 shadow-lg shadow-zinc-500/50 flex flex-col items-start justify-end pb-5 gap-2";
 
     const $num = document.createElement("p");
     $num.textContent = "#### #### #### ####";
-    $num.className = 'text-purple-800 text-3xl w-max p-3'
+    $num.className = "text-purple-800 text-3xl w-max p-3";
+
     const $name = document.createElement("p");
     $name.className = "text-purple-800 text-2xl p-3";
-    $name.textContent = 'Johan Sebastian'
+    $name.textContent = "Johan Sebastian";
 
     const $containerForm = document.createElement("div");
 
     $container.appendChild($card).appendChild($num);
-    $card.appendChild($name), $container.appendChild($containerForm);
+    $card.appendChild($name);
+    $container.appendChild($containerForm);
     $root.appendChild($container);
     return $container;
   }
